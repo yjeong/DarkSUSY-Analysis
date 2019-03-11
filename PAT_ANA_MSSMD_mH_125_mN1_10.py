8f8c0ec5853daa29323288b3f4773325c1f1823e
YongHo, Jeong <yonghojeong57@gmail.com>
2019-03-11 19:45:49 +0100

    Add files via upload

--- PAT_ANA_MSSMD_mH_125_mN1_10.py
+++ PAT_ANA_MSSMD_mH_125_mN1_10.py
@@ -9,7 +9,12 @@ config = config()
 #config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_0p4_cT_0'
 #config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_5_cT_50'
 #config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_0p4_cT_0p05'
-config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_2_cT_5'
+#config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_2_cT_5'
+#config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_2_cT_1'
+#---
+#config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_0p25_cT_0p1'
+config.General.requestName = 'MSSMD_mH_125_mN1_10_mGammaD_5_cT_10'
+
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.JobType.pluginName = 'Analysis'
@@ -24,7 +29,11 @@ config.JobType.allowUndistributedCMSSW = True
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_0p4_cT_0_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_5_cT_50_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_0p4_cT_0p05_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
-config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_2_cT_5_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_2_cT_5_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_2_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#---
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_0p25_cT_0p1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+config.Data.inputDataset = '/MSSMD_mH_125_mN1_10_mGammaD_5_cT_10_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
