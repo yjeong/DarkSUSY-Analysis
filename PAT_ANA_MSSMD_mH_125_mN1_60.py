ee74f9e1cba540302a791b422168d7d995a86839
YongHo, Jeong <yonghojeong57@gmail.com>
2019-03-21 16:41:22 +0100

    Add files via upload

--- PAT_ANA_MSSMD_mH_125_mN1_60.py
+++ PAT_ANA_MSSMD_mH_125_mN1_60.py
@@ -25,8 +25,19 @@ config = config()
 #config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_15_cT_10'
 #---
 #config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_5_cT_1'
-config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_15_cT_1'
-
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_15_cT_1'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_0p7_cT_10'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_2_cT_0p1'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_5_cT_0'
+#---
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_25_cT_0p1'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_25_cT_0'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_10_cT_1'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_25_cT_10'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_58_cT_5'
+#---
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_10_cT_0'
+config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_0p25_cT_0p1'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.JobType.pluginName = 'Analysis'
@@ -57,7 +68,19 @@ config.JobType.allowUndistributedCMSSW = True
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_15_cT_10_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 #---
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_5_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
-config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_15_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_15_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_0p7_cT_10_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_2_cT_0p1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_5_cT_0_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#---
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_25_cT_0p1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_25_cT_0_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_10_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_25_cT_10_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_58_cT_5_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#---
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_10_cT_0_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_0p25_cT_0p1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
