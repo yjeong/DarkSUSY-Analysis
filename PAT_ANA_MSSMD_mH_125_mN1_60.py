f2d30799c8b6fe87058fa06f3924c09e9adaf0cf
YongHo, Jeong <yonghojeong57@gmail.com>
2019-03-28 17:34:40 +0100

    Add files via upload

--- PAT_ANA_MSSMD_mH_125_mN1_60.py
+++ PAT_ANA_MSSMD_mH_125_mN1_60.py
@@ -42,7 +42,8 @@ config = config()
 #config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_15_cT_0'
 #config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_58_cT_1'
 #---
-config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_2_cT_3'
+#config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_2_cT_3'
+config.General.requestName = 'MSSMD_mH_125_mN1_60_mGammaD_35_cT_100'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.JobType.pluginName = 'Analysis'
@@ -90,7 +91,8 @@ config.JobType.allowUndistributedCMSSW = True
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_15_cT_0_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 #config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_58_cT_1_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 #---
-config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_2_cT_3_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_2_cT_3_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+config.Data.inputDataset = '/MSSMD_mH_125_mN1_60_mGammaD_35_cT_100_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
